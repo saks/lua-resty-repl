e84fdeb54994efc0db962d2772f097e36c9f7807
Aliaksandr Rahalevich <saksmlz@gmail.com>
2016-09-20 01:16:09 +0800

    make sure we can write to the history file
    - fixes #12
    - don't write failed command into history

--- lib/resty/repl/readline_stub.lua
+++ lib/resty/repl/readline_stub.lua
@@ -14,6 +14,9 @@ end
 local function set_attempted_completion_function()
 end
 
+local function add_to_history()
+end
+
 local function readline(prompt)
   io.write(prompt)
   io.stdout:flush()
@@ -25,6 +28,7 @@ local _M = setmetatable({
   teardown = teardown,
   puts = puts,
   set_attempted_completion_function = set_attempted_completion_function,
+  add_to_history = add_to_history,
 }, { __call = function(_, ...) return readline(...) end })
 
 return _M
